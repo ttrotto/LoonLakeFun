7d2a24edf1bb568f8e0bbbee0932f2feed5181c1
ttrotto <armityle@outlook.it>
2024-06-05 17:23:06 -0700

    deploy streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import matplotlib.pyplot as plt
+import time
+
+st.write("""
+# Live Dashboard
+Pick attribute of interest:
+""")
+
+# options
+option = st.selectbox(
+    "Attribute",
+    ("height", "dbh")
+)
+
+# container
+placeholder = st.empty()
+
+# fetch data
+def fetch():
+    conn = st.connection("gsheets",
+                         type=GSheetsConnection,
+                         ttl=5)
+    df = conn.read(ttl=5)
+    return df
+
+# refresh data
+for i in range(10):
+    df = fetch()
+    print(df)
+    with placeholder.container():
+        fig, ax = plt.subplots()
+        ax.hist(df[option])
+        st.pyplot(fig)
+        time.sleep(5)
