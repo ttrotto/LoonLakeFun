fa6a06b4f2bc36a19d4e92da6e3952427b28d5a9
ttrotto <armityle@outlook.it>
2024-06-25 15:53:46 -0700

    updated index and streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,18 @@ import time
 def fetch():
     conn = st.connection("gsheets",
                          type=GSheetsConnection,
-                         ttl=5)
-    df = conn.read(ttl=5)
+                         ttl=15)
+    df = conn.read(ttl=15)
     return df
 
+def plot_me(df, option):
+    fig, ax = plt.subplots()
+    ax.hist(df[option])
+    st.pyplot(fig)
+    # time.sleep(5)
 
+
+# <-- Init page --> #
 st.write("""
 # Live Dashboard
 Pick attribute of interest:
@@ -28,11 +35,7 @@ option = st.selectbox(
 placeholder = st.empty()
 
 # refresh data
-while True:
+if st.buttom("Refresh"):
     df = fetch()
-    print(df)
     with placeholder.container():
-        fig, ax = plt.subplots()
-        ax.hist(df[option])
-        st.pyplot(fig)
-        time.sleep(5)
+        plot_me(df, option)
